8f2bbee0560a884b73f750723d6c828ab5eda493
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-16 15:10:27 +0600

    1020 init

---
+++ URI/1020.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class _1020
+    {
+        public static void run()
+        {
+            int age = Convert.ToInt32(Console.ReadLine());
+            int year = age / 365;
+            Console.WriteLine(year + " ano(s)");
+            int remainder = age % 365;
+            int month = remainder / 30;
+            Console.WriteLine(month + " mes(es)");
+            int days = remainder - month * 30;
+            Console.WriteLine(days + " dia(s)");
+
+        }
+    }
+}
