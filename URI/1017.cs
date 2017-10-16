5f8b0aab68b3a0fe27398674436d26b73bafd59c
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-16 15:07:54 +0600

    1017 init

---
+++ URI/1017.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class _1017
+    {
+        public static void run()
+        {
+            double hour = Convert.ToDouble(Console.ReadLine());
+            int kmPerHour = Convert.ToInt32(Console.ReadLine());
+            double fuel = (hour * kmPerHour) / 12;
+            Console.WriteLine(String.Format("{0:0.000}", fuel));
+        }
+    }
+}
