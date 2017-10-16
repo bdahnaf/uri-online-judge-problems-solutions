bcf95d8f324b94e74d47461df0ddd0a7a31479bc
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-16 14:21:24 +0600

    1008 init

---
+++ URI/1008.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class _1008
+    {
+        public static void run()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            double workHours = Convert.ToDouble(Console.ReadLine());
+            double amount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("NUMBER = " + number);
+            Console.WriteLine("SALARY = U$ {0}", String.Format("{0:0.00}", workHours * amount));
+        }
+    }
+}
