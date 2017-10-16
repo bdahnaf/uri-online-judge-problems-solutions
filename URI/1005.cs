10853d4ee6bb70b4bd0bdeedf7c8ab385295419c
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-16 13:32:03 +0600

    1005 init

---
+++ URI/1005.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class _1005
+    {
+        public static void run()
+        {
+            double firstInput;
+            double secondInput;
+            double result;
+            firstInput = Convert.ToDouble(Console.ReadLine());
+            secondInput = Convert.ToDouble(Console.ReadLine());
+            result = ((firstInput * 3.5) + (secondInput * 7.5)) / (3.5 + 7.5);
+            Console.WriteLine("MEDIA = {0}", Math.Round(result, 5));
+        }
+    }
+}
