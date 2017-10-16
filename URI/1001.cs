3965914eeb101610d8b181450d60219433328c5b
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-16 12:01:42 +0600

    Project init

---
+++ URI/1001.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class _1001
+    {
+        public static void Program()
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int sum = A + B;
+            Console.WriteLine("X = " + sum);
+        }
+    }
+}
---
+++ URI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call the corresponding class for a specific program
+            _1001.Program();
+        }
+    }
+}
