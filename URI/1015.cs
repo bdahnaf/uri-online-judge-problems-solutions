92a1e214bd401292193a141d51ccf1a9550cde4e
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-16 14:55:53 +0600

    1015 init

---
+++ URI/1015.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class _1015
+    {
+        public static void run()
+        {
+            String[] input1 = Console.ReadLine().Split();
+            double x1 = Convert.ToDouble(input1[0]);
+            double y1 = Convert.ToDouble(input1[1]);
+            String[] input2 = Console.ReadLine().Split();
+            double x2 = Convert.ToDouble(input2[0]);
+            double y2 = Convert.ToDouble(input2[1]);
+            double x = x2 - x1;
+            double y = y2 - y1;
+            double distance = (x * x) + (y * y);
+            distance = Math.Sqrt(distance);
+            Console.WriteLine(String.Format("{0:0.0000}", distance));
+        }
+    }
+}
