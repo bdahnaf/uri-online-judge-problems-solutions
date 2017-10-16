eba5994ddf171823aa3f139163a7c8523487b73e
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-16 15:06:56 +0600

    1016 init

---
+++ URI/1016.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class _1016
+    {
+        public static void run()
+        {
+            int km = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} minutos", km * 2);
+        }
+    }
+}
