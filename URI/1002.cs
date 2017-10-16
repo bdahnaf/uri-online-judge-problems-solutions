0a3278570f691693d95dedfa531ebbe064327f22
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-16 13:13:15 +0600

    1002 init

---
+++ URI/1002.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace URI
+{
+    class _1002
+    {
+        public static void run()
+        {
+            double R = Convert.ToDouble(Console.ReadLine());
+            double area = 3.14159 * R * R;
+            Console.WriteLine("A=" + area.ToString("0.0000"));
+        }
+    }
+}
