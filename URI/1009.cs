d7d51f79a5067cd49627c3f6fe437794490092fb
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-16 14:23:05 +0600

    1009 init

---
+++ URI/1009.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class _1009
+    {
+        public static void run()
+        {
+            string name = Console.ReadLine();
+            double salary = Convert.ToDouble(Console.ReadLine());
+            double sales = Convert.ToDouble(Console.ReadLine());
+            double finalSalary = salary + sales * .15;
+            Console.WriteLine("TOTAL = R$ " + String.Format("{0:0.00}", finalSalary));
+        }
+    }
+}
