5eac94ad4c1c81ebfc43561057ce993b8e13ae9e
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-16 14:18:59 +0600

    1006 & 1007 init

---
+++ URI/1007.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class _1007
+    {
+        public static void run()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("DIFERENCA = {0}", (a * b - c * d));
+        }
+    }
+}
