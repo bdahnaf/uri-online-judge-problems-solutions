01aec02eee5b34c55cf80032462ccc8d93450616
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-16 13:31:12 +0600

    1004 init

---
+++ URI/1004.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class _1004
+    {
+        public static void run()
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int ans = A * B;
+            Console.WriteLine("PROD = " + ans);
+        }
+    }
+}
