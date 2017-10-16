448d7044761268c5ab77673c9ac47d8818fdab91
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-16 15:09:38 +0600

    1019 init

---
+++ URI/1019.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class _1019
+    {
+        public static void run()
+        {
+            int seconds = Convert.ToInt32(Console.ReadLine());
+            int hours = seconds / 3600;
+            int remainders = seconds % 3600;
+            int minutes = remainders / 60;
+            seconds = remainders - (minutes * 60);
+            Console.WriteLine(hours + ":" + minutes + ":" + seconds);
+        }
+    }
+}
