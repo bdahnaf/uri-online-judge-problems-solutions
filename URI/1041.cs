f6ea28c27414e5f15fdd636c05124ac9efdfbdf0
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-26 10:39:36 +0600

    Added 1041

---
+++ URI/1041.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace URI
+{
+    class _1041
+    {
+        public static void run()
+        {
+            String[] input = Console.ReadLine().Split();
+            double x = Convert.ToDouble(input[0]);
+            double y = Convert.ToDouble(input[1]);
+
+            Console.ReadKey();
+        }
+    }
+}
