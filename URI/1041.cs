62520ab13cc8e4590318611716bd224ddbb6a53d
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-26 10:53:51 +0600

    1041 AC

--- URI/1041.cs
+++ URI/1041.cs
@@ -9,8 +9,34 @@ namespace URI
             String[] input = Console.ReadLine().Split();
             double x = Convert.ToDouble(input[0]);
             double y = Convert.ToDouble(input[1]);
-
-            Console.ReadKey();
+            if(x == 0 && y == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (x == 0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+            else if (y == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
         }
     }
 }
