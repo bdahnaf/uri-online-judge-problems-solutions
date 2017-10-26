5ffe32daf9d8ce6b8c9ab4d16f73a56784c1d1f1
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-26 12:16:06 +0600

    1043 AC

--- URI/1043.cs
+++ URI/1043.cs
@@ -7,55 +7,17 @@ namespace URI
         public static void run()
         {
             String[] input = Console.ReadLine().Split();
-            int n1 = Convert.ToInt32(input[0]);
-            int n2 = Convert.ToInt32(input[1]);
-            int n3 = Convert.ToInt32(input[2]);
-            if (n1 >= n2 && n1 >= n3)
+            double n1 = Convert.ToDouble(input[0]);
+            double n2 = Convert.ToDouble(input[1]);
+            double n3 = Convert.ToDouble(input[2]);
+            if(n1+n2 > n3 && n2+n3 > n1 && n3+n1 > n2)
             {
-                Console.WriteLine(n1);
-                if (n2 > n3)
-                {
-                    Console.WriteLine(n2);
-                    Console.WriteLine(n3);
-                }
-                else
-                {
-                    Console.WriteLine(n3);
-                    Console.WriteLine(n2);
-                }
-            }
-            else if (n2 >= n1 && n2 >= n3)
-            {
-                Console.WriteLine(n2);
-                if (n1 > n3)
-                {
-                    Console.WriteLine(n1);
-                    Console.WriteLine(n3);
-                }
-                else
-                {
-                    Console.WriteLine(n3);
-                    Console.WriteLine(n1);
-                }
+                Console.WriteLine("Perimetro = " + String.Format("{0:0.0}", n1 + n2 + n3));
             }
             else
             {
-                Console.WriteLine(n3);
-                if (n1 > n2)
-                {
-                    Console.WriteLine(n1);
-                    Console.WriteLine(n2);
-                }
-                else
-                {
-                    Console.WriteLine(n2);
-                    Console.WriteLine(n1);
-                }
+                Console.WriteLine("Area = " + String.Format("{0:0.0}", ((n1 + n2) / 2) * n3));
             }
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.ReadKey();
         }
     }
 }
