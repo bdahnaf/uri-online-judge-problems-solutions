8f08d0ed66e1c539a1f9c103697c3927f0462111
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-26 13:13:15 +0600

    1044 AC

---
+++ URI/1044.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace URI
+{
+    class _1044
+    {
+        public static void run()
+        {
+            String[] input = Console.ReadLine().Split();
+            int n1 = Convert.ToInt32(input[0]);
+            int n2 = Convert.ToInt32(input[1]);
+            if (n1 % n2 == 0 || n2 % n1 == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
