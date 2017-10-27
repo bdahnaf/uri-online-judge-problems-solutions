db30d0bd3aadb7eeb61e8bafda1c483148bacd03
Ahnaf Shahriar <bdahnaf@gmail.com>
2017-10-27 09:58:51 +0600

    1046 AC

--- URI/1046.cs
+++ URI/1046.cs
@@ -9,8 +9,16 @@ namespace URI
             String[] input = Console.ReadLine().Split();
             int t1 = Convert.ToInt32(input[0]);
             int t2 = Convert.ToInt32(input[1]);
-
-            
+            int answer = 0;
+            if(t1 < t2)
+            {
+                answer = t2 - t1;
+            }
+            else
+            {
+                answer = 24 - t1 + t2;
+            }
+            Console.WriteLine("O JOGO DUROU " + answer + " HORA(S)");
         }
     }
 }
