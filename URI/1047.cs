873b173a47b626309a70e216f65a50c81362e095
Ahnaf Shahriar <bdahnaf@gmail.com>
2017-10-27 10:48:33 +0600

    Added 1047

---
+++ URI/1047.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace URI
+{
+    class _1047
+    {
+        public static void run()
+        {
+            String[] input = Console.ReadLine().Split();
+            int t1 = Convert.ToInt32(input[0]);
+            int m1 = Convert.ToInt32(input[1]);
+            int t2 = Convert.ToInt32(input[2]);
+            int m2 = Convert.ToInt32(input[3]);
+
+
+        }
+    }
+}
