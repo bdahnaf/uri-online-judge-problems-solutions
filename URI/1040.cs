7d4e45f0b02f44be9890227d433e7174d6fe0684
Ahnaf Shahriar <bdahnaf@gmail.com>
2017-10-27 08:17:44 +0600

    daily commit

--- URI/1040.cs
+++ URI/1040.cs
@@ -13,14 +13,28 @@ namespace URI
             double n4 = Convert.ToDouble(input[3]);
             double average = ((n1 * 2) + (n2 * 3) + (n3 * 4) + n4) / 10;
             Console.WriteLine("Media: " + String.Format("{0:0.0}", average));
-            if(average >= 7.0)
+
+            if (average >= 7.0)
             {
+                
                 Console.WriteLine("Aluno aprovado.");
             }
-            else if (average >= 5 && average <= 6.9)
+            else if (average >= 5)
             {
-                Console.WriteLine("Aluno em exame.");
                 double n5 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Aluno em exame.");
+                
+                Console.WriteLine("Nota do exame: " + String.Format("{0:0.0}", n5));
+                average = (average + n5) / 2;
+                if(average >= 5.0)
+                {
+                    Console.WriteLine("Aluno aprovado.");
+                }
+                else if(average < 5.0)
+                {
+                    Console.WriteLine("Aluno reprovado.");
+                }
+                Console.WriteLine("Media final: " + String.Format("{0:0.0}", average));
 
             }
             else if (average < 5)
