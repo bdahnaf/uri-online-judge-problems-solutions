cb32402535101bda19e597e052f54d7d5552736f
Ahnaf Shahriar <bdahnaf@gmail.com>
2017-10-27 08:24:36 +0600

    1046 Added

---
+++ URI/1046.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace URI
+{
+    class _1046
+    {
+        public static void run()
+        {
+            String[] input = Console.ReadLine().Split();
+            int t1 = Convert.ToInt32(input[0]);
+            int t2 = Convert.ToInt32(input[1]);
+
+            
+        }
+    }
+}
