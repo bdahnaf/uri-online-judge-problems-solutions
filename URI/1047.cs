e8477b1c870340e702cbee2a76af2cef2e2cb834
Ahnaf Shahriar <bdahnaf@gmail.com>
2017-10-30 20:32:44 +0600

    Daily update

--- URI/1047.cs
+++ URI/1047.cs
@@ -11,8 +11,35 @@ namespace URI
             int m1 = Convert.ToInt32(input[1]);
             int t2 = Convert.ToInt32(input[2]);
             int m2 = Convert.ToInt32(input[3]);
-
-
+            int hour1 = 0;
+            int hour2 = 0;
+            int minute = 0;
+            /*
+            if (t1 < t2)
+            {
+                hour = t2 - t1;
+            }
+            else
+            {
+                hour = 24 - t1 + t2;
+            }
+            if (m1 < m2)
+            {
+                minute = m2 - m1;
+            }
+            else
+            {
+                minute = 60 - m1 + m2;
+            }
+            */
+            hour1 = t1 * 60 + m1;
+            hour2 = t2 * 60 + m2;
+            //minute = Math.Abs(hour1-hour2);
+            minute = hour1 - hour2;
+            int hour = minute / 60;
+            minute = minute % 60;
+            Console.WriteLine("O JOGO DUROU " + hour + " HORA(S) E " + minute + " MINUTO(S)");
+            Console.ReadKey();
         }
     }
 }
--- URI/Program.cs
+++ URI/Program.cs
@@ -7,7 +7,7 @@ namespace URI
         static void Main(string[] args)
         {
             //Call the corresponding class for a specific program
-            _1046.run();
+            _1047.run();
         }
     }
 }
