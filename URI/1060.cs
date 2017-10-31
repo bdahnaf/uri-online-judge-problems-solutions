968549bd845dae37b2ae0beafb4aad8edd842206
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-31 16:46:33 +0600

    1060 Added

---
+++ URI/1060.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace URI
+{
+    class _1060
+    {
+        public static void run()
+        {
+            String[] input = Console.ReadLine().Split();
+            int n1 = Convert.ToInt32(input[0]);
+            int n2 = Convert.ToInt32(input[1]);
+            int n3 = Convert.ToInt32(input[2]);
+            int n4 = Convert.ToInt32(input[3]);
+            int n5 = Convert.ToInt32(input[4]);
+            int n6 = Convert.ToInt32(input[5]);
+            int n = 0;
+            int count = 0;
+            while ((n = Convert.ToInt32(input[count]))%2 == 0)
+            {
+
+            }
+        }
+    }
+}
