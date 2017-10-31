ef4dc7e935bbdcbe11bdb0b5435376ba7e60ac2d
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-31 15:09:29 +0600

    2618 AC

---
+++ URI/SQL/2618.sql
@@ -0,0 +1,8 @@
+﻿SELECT prd.name, prv.name, cat.name
+FROM products prd, providers prv, categories cat
+WHERE
+cat.name = 'Imported' AND prv.name = 'Sansul SA'
+AND
+prd.id_categories IN (SELECT id FROM categories WHERE name = 'Imported')
+AND
+prd.id_providers IN (SELECT id FROM providers WHERE name = 'Sansul SA')
