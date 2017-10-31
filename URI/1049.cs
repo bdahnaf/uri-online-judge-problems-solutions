95904793d14e2fc263698548905b825555270b48
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-10-31 11:06:48 +0600

    1049 AC

--- URI/1049.cs
+++ URI/1049.cs
@@ -4,5 +4,65 @@ namespace URI
 {
     class _1049
     {
+        public static void run()
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string input3 = Console.ReadLine();
+            if(input1 == "vertebrado")
+            {
+                if(input2 == "ave")
+                {
+                    if(input3 == "carnivoro")
+                    {
+                        Console.WriteLine("aguia");
+                    }
+                    else
+                    {
+                        Console.WriteLine("pomba");
+                    }
+
+
+                }
+                else
+                {
+                    if (input3 == "onivoro")
+                    {
+                        Console.WriteLine("homem");
+                    }
+                    else
+                    {
+                        Console.WriteLine("vaca");
+                    }
+                }
+            }
+            else
+            {
+                if (input2 == "inseto")
+                {
+                    if (input3 == "hematofago")
+                    {
+                        Console.WriteLine("pulga");
+                    }
+                    else
+                    {
+                        Console.WriteLine("lagarta");
+                    }
+
+
+                }
+                else
+                {
+                    if (input3 == "hematofago")
+                    {
+                        Console.WriteLine("sanguessuga");
+                    }
+                    else
+                    {
+                        Console.WriteLine("minhoca");
+                    }
+                }
+            }
+        }
     }
 }
