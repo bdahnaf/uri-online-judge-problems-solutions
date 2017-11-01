f12fdf53591ffcdd9ae57a3b3dd0a799a70ae7f3
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-11-01 10:29:39 +0600

    1060 AC

--- URI/1060.cs
+++ URI/1060.cs
@@ -6,19 +6,18 @@ namespace URI
     {
         public static void run()
         {
-            String[] input = Console.ReadLine().Split();
-            int n1 = Convert.ToInt32(input[0]);
-            int n2 = Convert.ToInt32(input[1]);
-            int n3 = Convert.ToInt32(input[2]);
-            int n4 = Convert.ToInt32(input[3]);
-            int n5 = Convert.ToInt32(input[4]);
-            int n6 = Convert.ToInt32(input[5]);
-            int n = 0;
+            int loopCount = 1;
             int count = 0;
-            while ((n = Convert.ToInt32(input[count]))%2 == 0)
+            while(loopCount < 7)
             {
-
+                double number = Convert.ToDouble(Console.ReadLine());
+                if (number >= 0)
+                {
+                    count++;
+                }
+                loopCount++;
             }
+            Console.WriteLine(count + " valores positivos");
         }
     }
 }
