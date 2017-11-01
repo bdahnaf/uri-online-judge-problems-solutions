e86c6b4eafdd459f93c4ad8431d7457df44a0e80
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-11-01 15:33:03 +0600

    1078 Added

---
+++ URI/1078.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace URI
+{
+    class _1078
+    {
+        public static void run()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i + " x " + number + " = " + i*number);
+            }
+        }
+    }
+}
