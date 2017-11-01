6eacbc4143ae65e5be5be9916c1721b5b0089708
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-11-01 15:34:07 +0600

    1078 AC

--- URI/1078.cs
+++ URI/1078.cs
@@ -9,7 +9,7 @@ namespace URI
             int number = Convert.ToInt32(Console.ReadLine());
             for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine(i + " x " + number + " = " + i*number);
+                Console.WriteLine(i + " x " + number + " = " + i * number);
             }
         }
     }
