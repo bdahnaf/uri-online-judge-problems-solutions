02156146185d5f7138f8b33f9a6761f5de57bae2
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-11-01 11:49:33 +0600

    1065, 1066, 1067 AC

---
+++ URI/1065.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace URI
+{
+    class _1065
+    {
+        public static void run()
+        {
+            int loopCount = 1;
+            int count = 0;
+            while (loopCount < 6)
+            {
+                double number = Convert.ToDouble(Console.ReadLine());
+                if (number % 2 == 0)
+                {
+                    count++;
+                }
+                loopCount++;
+            }
+            Console.WriteLine(count + " valores pares");
+        }
+    }
+}
--- URI/Program.cs
+++ URI/Program.cs
@@ -7,7 +7,7 @@ namespace URI
         static void Main(string[] args)
         {
             //Call the corresponding class for a specific program
-            _1064.run();
+            _1067.run();
         }
     }
 }
