fe1b88da693f00ba036ac04893a5e8d3ba19c38d
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-11-01 13:45:22 +0600

    1073 Added

--- URI/1073.cs
+++ URI/1073.cs
@@ -7,7 +7,13 @@ namespace URI
         public static void run()
         {
             int range = Convert.ToInt32(Console.ReadLine());
-            
+            for(int i = 1; i <= range; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i + "^2" + " = " + i*i);
+                }
+            }
         }
     }
 }
