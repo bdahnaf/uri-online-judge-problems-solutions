8a7745900184f47a789a28c528789836ebc11e51
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-11-01 12:20:41 +0600

    1071 Added

---
+++ URI/1071.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace URI
+{
+    class _1071
+    {
+        public static void run()
+        {
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
