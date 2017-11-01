5b26a4945d23c4c42afbe36e6858ae485d5c54ad
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-11-01 13:16:35 +0600

    1071 AC

--- URI/Program.cs
+++ URI/Program.cs
@@ -7,7 +7,7 @@ namespace URI
         static void Main(string[] args)
         {
             //Call the corresponding class for a specific program
-            _1070.run();
+            _1071.run();
         }
     }
 }
