0e30154b29ad5b5ac8772180c1fc32e2bcb8c22c
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-11-01 15:02:06 +0600

    1074 AC

--- URI/1074.cs
+++ URI/1074.cs
@@ -7,7 +7,48 @@ namespace URI
         public static void run()
         {
             int range = Convert.ToInt32(Console.ReadLine());
-            
+            int []input = new int[range];
+            string[] output = new string[range];
+            for(int i = 0; i < range;  i++)
+            {
+                //int number = Convert.ToInt32(Console.ReadLine());
+                input[i] = Convert.ToInt32(Console.ReadLine());
+                if (input[i] == 0)
+                {
+                    //Console.WriteLine("NULL");
+                    output[i] = "NULL";
+                }
+                else if (input[i] % 2 == 0)
+                {
+                    if (input[i] > 0)
+                    {
+                        //Console.WriteLine("EVEN POSITIVE");
+                        output[i] = "EVEN POSITIVE";
+                    }
+                    else
+                    {
+                        //Console.WriteLine("EVEN NEGATIVE");
+                        output[i] = "EVEN NEGATIVE";
+                    }
+                }
+                else
+                {
+                    if (input[i] > 0)
+                    {
+                        output[i] = "ODD POSITIVE";
+                        //Console.WriteLine("ODD POSITIVE");
+                    }
+                    else
+                    {
+                        output[i] = "ODD NEGATIVE";
+                        //Console.WriteLine("ODD NEGATIVE");
+                    }
+                }
+            }
+            for(int i = 0; i < range; i++)
+            {
+                Console.WriteLine(output[i]);
+            }
         }
     }
 }
