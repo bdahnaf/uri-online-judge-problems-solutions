ccf669ca7c40d192169fddff7d4327821d77ce02
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-11-01 13:46:23 +0600

    1074 Added

---
+++ URI/1074.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace URI
+{
+    class _1074
+    {
+        public static void run()
+        {
+            int range = Convert.ToInt32(Console.ReadLine());
+            
+        }
+    }
+}
