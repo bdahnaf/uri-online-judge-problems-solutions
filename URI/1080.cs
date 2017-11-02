450a8813b77b1c1251180d1df1719cb7faa46dcc
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-11-02 10:48:41 +0600

    1080 Added

---
+++ URI/1080.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace URI
+{
+    class _1080
+    {
+        public static void run()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                
+            }
+        }
+    }
+}
