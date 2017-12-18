003b566a030fc80b25f980b4391379c51722e244
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-18 13:35:04 +0600

    1115 AC

--- URI/1115.cs
+++ URI/1115.cs
@@ -6,7 +6,30 @@ namespace URI
     {
         public static void run()
         {
-            
+            int number1, number2;
+            while (true)
+            {
+                String[] input = Console.ReadLine().Split();
+                number1 = Convert.ToInt32(input[0]);
+                number2 = Convert.ToInt32(input[1]);
+                if (number2 > 0 && number1 > 0)
+                {
+                    Console.WriteLine("primeiro");
+                }
+                else if (number1 > 0 && number2 < 0)
+                {
+                    Console.WriteLine("quarto");
+                }
+                else if (number1 < 0 && number2 < 0)
+                {
+                    Console.WriteLine("terceiro");
+                }
+                else if (number1 < 0 && number2 > 0)
+                {
+                    Console.WriteLine("segundo");
+                }
+                else return;
+            }
         }
     }
 }
