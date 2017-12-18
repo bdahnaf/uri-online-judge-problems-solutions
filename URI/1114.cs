cb968d6f97a9680b03452644e78213efa957dc3e
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-18 11:06:44 +0600

    1114 AC

--- URI/1114.cs
+++ URI/1114.cs
@@ -6,7 +6,13 @@ namespace URI
     {
         public static void run()
         {
-            
+            int input = Convert.ToInt32(Console.ReadLine());
+            while(input != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
         }
     }
 }
