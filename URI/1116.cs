c746a9ba37daba605c8661af4f2bfdfe2e7561ab
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-20 09:14:52 +0600

    AC

---
+++ URI/1116.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace URI
+{
+    class _1116
+    {
+        public static void run()
+        {
+            int loop;
+            loop = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < loop; i++)
+            {
+                String[] input = Console.ReadLine().Split();
+                double number1 = Convert.ToDouble(input[0]);
+                double number2 = Convert.ToDouble(input[1]);
+                double result = number1 / number2;
+                if(number2 == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    Console.WriteLine(String.Format("{0:0.0}", result));
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ URI/1117.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace URI
+{
+    class _1117
+    {
+        public static void run()
+        {
+            int count = 0;
+            double workingNumber = 0;
+            double result;
+            while (true)
+            {
+                double number = Convert.ToDouble(Console.ReadLine());
+                if(number >= 0.0 && number <= 10)
+                {
+                    count++;
+                    workingNumber = workingNumber + number;
+                    if (count == 2)
+                    {
+                        result = workingNumber / 2;
+                        Console.WriteLine("media = " + String.Format("{0:0.00}", result));
+                        return;
+                    }
+                }
+                if(number < 0 || number > 10)
+                {
+                    Console.WriteLine("nota invalida");
+                }
+                
+            }
+        }
+    }
+}
