a03a4e119c1524780c02ebb433a43548583e4825
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-21 10:13:02 +0600

    1133 AC

--- URI/1133.cs
+++ URI/1133.cs
@@ -6,9 +6,23 @@ namespace URI
     {
         public static void run()
         {
-            int number = 0;
-            int alcohol, gasolin, diesel;
-            
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            if(number1 > number2)
+            {
+                number1 = number1 + number2;
+                number2 = number1 - number2;
+                number1 = number1 - number2;
+            }
+            number1++;
+            for(int i = number1; i < number2; i++)
+            {
+                int number = i;
+                if(number%5==2 || number % 5 == 3)
+                {
+                    Console.WriteLine(number);
+                }
+            }
         }
     }
 }
