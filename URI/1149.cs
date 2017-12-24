99baf7c433c72a4ab9a32f8fd2b752fd5875490c
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-24 14:42:49 +0600

    1149 Try

--- URI/1149.cs
+++ URI/1149.cs
@@ -4,5 +4,29 @@ namespace URI
 {
     class _1149
     {
+        public static void run()
+        {
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+                int A = Convert.ToInt32(input[0]);
+                int N = Convert.ToInt32(input[1]);
+                int counter = 0;
+                int answer = 0;
+                int i = A;
+                while (true)
+                {
+                    answer = answer + i;
+                    i++;
+                    counter++;
+                    if(counter == N)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine(answer);
+            }
+            Console.ReadKey();
+        }
     }
 }
--- URI/Program.cs
+++ URI/Program.cs
@@ -7,7 +7,7 @@ namespace URI
         static void Main(string[] args)
         {
             //Call the corresponding class for a specific program
-            _1146.run();
+            _1149.run();
         }
     }
 }
