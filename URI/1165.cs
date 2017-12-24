e5b83f9410dd1dec1a7a2b38b4a6822b09825168
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-24 09:59:13 +0600

    1165 AC

---
+++ URI/1165.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace URI
+{
+    class _1165
+    {
+        public static void run()
+        {
+            int counter = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < counter; i++)
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                for (int j = 2; j < number/2; j++)
+                {
+                    if(j % 2 == 0 || j % 3 == 0)
+                    {
+                        Console.WriteLine(number + " nao eh perfeito");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(number + " eh perfeito");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- URI/Program.cs
+++ URI/Program.cs
@@ -7,7 +7,7 @@ namespace URI
         static void Main(string[] args)
         {
             //Call the corresponding class for a specific program
-            _1133.run();
+            _1165.run();
         }
     }
 }
