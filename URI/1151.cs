628ab64502dac81d22ec9037b2dffa183790e106
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-26 16:56:28 +0600

    1151 Try

---
+++ URI/1151.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace URI
+{
+    class _1151
+    {
+        public static void run()
+        {
+            int limit = Convert.ToInt32(Console.ReadLine());
+            int number1 = 0;
+            int number2 = 1;
+            int temp = 0;
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+            for (int i = 3; i < limit; i++)
+            {
+                Console.WriteLine("Value of i ->" + i);
+                number2 = number1 + number2;
+                Console.WriteLine(number2);
+                Console.WriteLine();
+                Console.WriteLine(number1);
+                number1 = number2;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- URI/Program.cs
+++ URI/Program.cs
@@ -7,7 +7,7 @@ namespace URI
         static void Main(string[] args)
         {
             //Call the corresponding class for a specific program
-            _1150.run();
+            _1151.run();
         }
     }
 }
