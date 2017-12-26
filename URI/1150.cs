88f8679c60a557a7533afcbafa066aaaa6c8c1c5
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-26 12:33:53 +0600

    1150 Try

---
+++ URI/1150.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace URI
+{
+    class _1150
+    {
+        public static void run()
+        {
+            while (true)
+            {
+                int X = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
