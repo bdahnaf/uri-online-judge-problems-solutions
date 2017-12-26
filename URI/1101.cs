6321e08dcb8e909b74b7498a1a6f63c8b087b421
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-26 11:26:50 +0600

    1101 AC

---
+++ URI/1101.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace URI
+{
+    class _1101
+    {
+        public static void run()
+        {
+            while (true)
+            {
+                String[] input = Console.ReadLine().Split();
+                int M = Convert.ToInt32(input[0]);
+                int N = Convert.ToInt32(input[1]);
+                int sum = 0;
+                if (M <= 0 || N <= 0)
+                {
+                    return;
+                }
+                else if (M > N)
+                {
+                    for (int i = N; i <= M; i++)
+                    {
+                        Console.Write(i + " ");
+                        sum = sum + i;
+                    }
+                    Console.WriteLine("Sum=" + sum);
+                }
+                else
+                {
+                    for (int i = M; i <= N; i++)
+                    {
+                        Console.Write(i + " ");
+                        sum = sum + i;
+                    }
+                    Console.WriteLine("Sum=" + sum);
+                }
+            }
+        }
+    }
+}
--- URI/Program.cs
+++ URI/Program.cs
@@ -7,7 +7,7 @@ namespace URI
         static void Main(string[] args)
         {
             //Call the corresponding class for a specific program
-            _1113.run();
+            _1101.run();
         }
     }
 }
