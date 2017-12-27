4256cd2bc7efec255cbd23a66ff86231d67e4bc8
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-27 14:47:44 +0600

    1155 AC

---
+++ URI/1156.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace URI
+{
+    class _1156
+    {
+        public static void run()
+        {
+            double upper = 1;
+            double lower = 1;
+            double sum = 0;
+            while(upper != 39)
+            {
+                sum = sum + upper / lower;
+                upper = upper + 2;
+                lower = lower + lower;
+            }
+            Console.WriteLine(String.Format("{0:0.00}", sum));
+        }
+    }
+}
