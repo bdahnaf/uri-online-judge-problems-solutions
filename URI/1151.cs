1f77cb388c5f611d1b10de7187f96943cbddc0f1
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-27 12:03:14 +0600

    1151 AC

--- URI/1151.cs
+++ URI/1151.cs
@@ -9,20 +9,18 @@ namespace URI
             int limit = Convert.ToInt32(Console.ReadLine());
             int number1 = 0;
             int number2 = 1;
-            int temp = 0;
-            Console.WriteLine(number1);
-            Console.WriteLine(number2);
-            for (int i = 3; i < limit; i++)
+            int fibonacci = 0;
+            int counter = 3;
+            Console.Write(number1 + " " + number2);
+            while (counter <= limit)
             {
-                Console.WriteLine("Value of i ->" + i);
-                number2 = number1 + number2;
-                Console.WriteLine(number2);
-                Console.WriteLine();
-                Console.WriteLine(number1);
+                fibonacci = number1 + number2;
+                Console.Write(" " + fibonacci);
                 number1 = number2;
-
+                number2 = fibonacci;
+                counter++;
             }
-            Console.ReadKey();
+            Console.WriteLine();
         }
     }
 }
