cf162d68cc1f6a42446f97c4b30e7ed3345033d9
Ahnaf Shahriar <bdahnaf@gmail.com>
2017-12-28 00:19:11 +0600

    1165 Try

--- URI/1165.cs
+++ URI/1165.cs
@@ -10,17 +10,28 @@ namespace URI
             for(int i = 0; i < counter; i++)
             {
                 int number = Convert.ToInt32(Console.ReadLine());
-                for (int j = 2; j < number/2; j++)
+                if(number == 0 || number == 1 || number == 4)
                 {
-                    if(j % 2 == 0 || j % 3 == 0)
+                    Console.WriteLine(number + " nao eh primo");
+                }
+                else if(number == 2 || number == 3)
+                {
+                    Console.WriteLine(number + " eh primo");
+                }
+                else
+                {
+                    for (int j = 3; j < number; j++)
                     {
-                        Console.WriteLine(number + " nao eh perfeito");
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine(number + " eh perfeito");
-                        break;
+                        if (number % j == 0)
+                        {
+                            Console.WriteLine(number + " nao eh primo");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine(number + " eh primo");
+                            break;
+                        }
                     }
                 }
             }
