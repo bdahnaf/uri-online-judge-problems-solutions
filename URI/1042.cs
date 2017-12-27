494c02e56ee3b141a9a45a6bad2f0bfab68e48a5
Ahnaf Shahriar <bdahnaf@gmail.com>
2017-12-27 21:12:36 +0600

    1042 AC

--- URI/1042.cs
+++ URI/1042.cs
@@ -10,10 +10,10 @@ namespace URI
             int n1 = Convert.ToInt32(input[0]);
             int n2 = Convert.ToInt32(input[1]);
             int n3 = Convert.ToInt32(input[2]);
-            if (n1 >= n2 && n1 >= n3)
+            if(n1 <= n2 && n1 <= n3)
             {
                 Console.WriteLine(n1);
-                if(n2 > n3)
+                if(n2 < n3)
                 {
                     Console.WriteLine(n2);
                     Console.WriteLine(n3);
@@ -24,24 +24,24 @@ namespace URI
                     Console.WriteLine(n2);
                 }
             }
-            else if (n2 >= n1 && n2 >= n3)
+            else if (n2 <= n3 && n2 <= n1)
             {
                 Console.WriteLine(n2);
-                if(n1 > n3)
+                if(n3 < n1)
                 {
-                    Console.WriteLine(n1);
                     Console.WriteLine(n3);
+                    Console.WriteLine(n1);
                 }
                 else
                 {
-                    Console.WriteLine(n3);
                     Console.WriteLine(n1);
+                    Console.WriteLine(n3);
                 }
             }
-            else
+            else if (n3 <= n1 && n3 <= n2)
             {
                 Console.WriteLine(n3);
-                if (n1 > n2)
+                if(n1 < n2)
                 {
                     Console.WriteLine(n1);
                     Console.WriteLine(n2);
@@ -52,10 +52,10 @@ namespace URI
                     Console.WriteLine(n1);
                 }
             }
+            Console.WriteLine();
             Console.WriteLine(n1);
             Console.WriteLine(n2);
             Console.WriteLine(n3);
-            Console.ReadKey();
         }
     }
 }
