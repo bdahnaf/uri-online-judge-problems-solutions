46ced6269e7f2b7e3ff35f36623f05899708550f
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-27 12:12:25 +0600

    1153 AC

---
+++ URI/1153.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace URI
+{
+    class _1153
+    {
+        public static void run()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial = i * factorial;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
--- URI/Program.cs
+++ URI/Program.cs
@@ -7,7 +7,7 @@ namespace URI
         static void Main(string[] args)
         {
             //Call the corresponding class for a specific program
-            _1151.run();
+            _1153.run();
         }
     }
 }
