ee4730a69759d647bdebf0e67ee29b5c4082ed7a
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-27 16:14:07 +0600

    1158 AC

--- URI/1158.cs
+++ URI/1158.cs
@@ -6,13 +6,30 @@ namespace URI
     {
         public static void run()
         {
-            int number = Convert.ToInt32(Console.ReadLine());
-            for (int i = 1; i <= number; i++)
+            int counter = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (i < counter)
             {
-                if (number % i == 0)
+                String[] input = Console.ReadLine().Split();
+                int X = Convert.ToInt32(input[0]);
+                int Y = Convert.ToInt32(input[1]);
+                int j = 0;
+                int sum = 0;
+                while (true)
                 {
-                    Console.WriteLine(i);
+                    if (X % 2 != 0)
+                    {
+                        sum = sum + X;
+                        j++;
+                        if(j == Y)
+                        {
+                            break;
+                        }
+                    }
+                    X++;
                 }
+                Console.WriteLine(sum);
+                i++;
             }
         }
     }
