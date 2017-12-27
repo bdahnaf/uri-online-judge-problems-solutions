7786f92edee17d68c5cc352d0daca61e885d63e4
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-27 12:32:10 +0600

    1154 AC

---
+++ URI/1154.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace URI
+{
+    class _1154
+    {
+        public static void run()
+        {
+            double sum = 0;
+            int counter = 0;
+            while (true)
+            {
+                double number = Convert.ToDouble(Console.ReadLine());
+                if(number < 0)
+                {
+                    Console.WriteLine(String.Format("{0:0.00}", sum / counter));
+                    return;
+                }
+                else
+                {
+                    sum = sum + number;
+                    counter++;
+                }
+            }
+        }
+    }
+}
