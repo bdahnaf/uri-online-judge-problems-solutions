0ab7dcc4fdf8636ddd666d57699b06777f48b5d6
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-28 13:05:26 +0600

    1174 Add

---
+++ URI/1174.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace URI
+{
+    class _1174
+    {
+        public static void run()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int[] input = new int[10];
+            input[0] = number;
+            for (int i = 1; i < 10; i++)
+            {
+                input[i] = input[i - 1] * 2;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("N[" + i + "] = " + input[i]);
+            }
+        }
+    }
+}
--- URI/Program.cs
+++ URI/Program.cs
@@ -7,7 +7,7 @@ namespace URI
         static void Main(string[] args)
         {
             //Call the corresponding class for a specific program
-            _1173.run();
+            _1174.run();
         }
     }
 }
