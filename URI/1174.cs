ff2626407b31a51d1d07beec68dc42dacbc49cc4
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2017-12-28 13:52:29 +0600

    1174 AC

--- URI/1174.cs
+++ URI/1174.cs
@@ -6,16 +6,16 @@ namespace URI
     {
         public static void run()
         {
-            double[] input = new double[10];
-            for (int i = 0; i < 10; i++)
+            double[] input = new double[100];
+            for (int i = 0; i < 100; i++)
             {
                 input[i] = Convert.ToDouble(Console.ReadLine());
             }
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 100; i++)
             {
                 if(input[i] <= 10)
                 {
-                    Console.WriteLine("A[" + i + "] = " + input[i]);
+                    Console.WriteLine("A[" + i + "] = " + String.Format("{0:0.0}", input[i]));
                 }
             }
         }
