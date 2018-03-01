5f5922d9f52d149e094e5d8c2dc03ebf505e8fad
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2018-03-01 15:38:48 +0600

    1865 AC

--- URI/1865.cs
+++ URI/1865.cs
@@ -7,7 +7,30 @@ namespace URI
     {
         public static void run()
         {
-            
+            int counter = Convert.ToInt32(Console.ReadLine());
+            string output = "";
+            string superhero = Console.ReadLine();
+            if (superhero.Substring(0, 4) == "Thor")
+            {
+                output = output + "Y";
+            }
+            else
+            {
+                output = output + "N";
+            }
+            for (int i = 1; i < counter; i++)
+            {
+                superhero = Console.ReadLine();
+                if (superhero.Substring(0, 4) == "Thor")
+                {
+                    output = output + "\nY";
+                }
+                else
+                {
+                    output = output + "\nN";
+                }
+            }
+            Console.WriteLine(output);
         }
     }
 }
