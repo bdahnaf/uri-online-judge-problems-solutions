3ca6e7276646ef8adb259f7e443ae773485fe903
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2018-03-01 11:54:59 +0600

    1759 AC

---
+++ URI/1759.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class _1759
+    {
+        public static void run()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int counter = 1;
+            while(counter < number)
+            {
+                Console.Write("Ho ");
+                counter++;
+            }
+            Console.WriteLine("Ho!");
+        }
+    }
+}
