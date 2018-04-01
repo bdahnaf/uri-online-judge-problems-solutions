d01113d2429f3c2f2cab66721a609e74d2ebca35
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2018-04-01 11:47:26 +0600

    2031 AC

--- URI/2031.cs
+++ URI/2031.cs
@@ -6,7 +6,52 @@ namespace URI
     {
         public static void run()
         {
-            
+            int range = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < range; i++)
+            {
+                string p1 = Console.ReadLine();
+                string p2 = Console.ReadLine();
+                if (p1 == "pedra" && p2 == "pedra")
+                {
+                    Console.WriteLine("Sem ganhador");
+                }
+                else if(p1 == "papel" && p2 == "papel")
+                {
+                    Console.WriteLine("Ambos venceram");
+                }
+                else if (p1 == "ataque" && p2 == "ataque")
+                {
+                    Console.WriteLine("Aniquilacao mutua");
+                }
+                else if (p1 == "ataque" && p2 == "ataque")
+                {
+                    Console.WriteLine("Aniquilacao mutua");
+                }
+                else if (p1 == "ataque" && p2 == "pedra")
+                {
+                    Console.WriteLine("Jogador 1 venceu");
+                }
+                else if (p1 == "pedra" && p2 == "ataque")
+                {
+                    Console.WriteLine("Jogador 2 venceu");
+                }
+                else if (p1 == "pedra" && p2 == "papel")
+                {
+                    Console.WriteLine("Jogador 1 venceu");
+                }
+                else if (p1 == "papel" && p2 == "pedra")
+                {
+                    Console.WriteLine("Jogador 2 venceu");
+                }
+                else if (p1 == "papel" && p2 == "ataque")
+                {
+                    Console.WriteLine("Jogador 2 venceu");
+                }
+                else if (p1 == "ataque" && p2 == "papel")
+                {
+                    Console.WriteLine("Jogador 1 venceu");
+                }
+            }
         }
     }
 }
