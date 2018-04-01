00bda2a5b8c4d6107c812cb5bf0b57a184a2d984
Ahnaf Shahriar <Ahnaf Shahriar@DESKTOP-F54KLHV>
2018-04-01 10:58:34 +0600

    2031 Added

---
+++ URI/2031.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace URI
+{
+    class _2031
+    {
+        public static void run()
+        {
+            
+        }
+    }
+}
--- URI/Program.cs
+++ URI/Program.cs
@@ -7,7 +7,7 @@ namespace URI
         static void Main(string[] args)
         {
             //Call the corresponding class for a specific program
-            _2006.run();
+            _2031.run();
         }
     }
 }
